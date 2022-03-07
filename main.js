d7d5a638e417774a6510a30210b4658c85579fa3
ubuntu <ubuntu@ik1-412-38278.vs.sakura.ne.jp>
2022-03-08 04:18:33 +0900

    update

--- main.js
+++ main.js
@@ -1,60 +1,59 @@
-const discord = require("discord.js");
+'use strict';
+
+require('dotenv').config()
+const discord = require('discord.js');
+
 const client = new discord.Client();
 
-let channelsNotify = [];
-
-client.login(process.env.discord_bot_token);
-
-client.on("ready", message => {
-  console.log("ready");
-
-  channelsNotify = client.channels.cache
-    .array()
-    .filter(
-      channel =>
-        channel.type === "text" &&
-        channel.topic &&
-        channel.topic.includes("/vc-notify")
-    );
-
-  console.log("/ready");
+const tcs = [];
+client.on('ready', message => {
+  for (const channel of client.channels.cache)
+    if (channel.type == 'text' && channel.topic) {
+      const vcs = topic.match(/(?<=!vc-notify:)[0-9,]+/);
+      if (vcs)
+        tcs.push({
+          tc: channel,
+          vcIds: vcs.split(',').map(parseInt),
+        });
+    }
 });
 
-client.on("voiceStateUpdate", (stateOld, stateNew) => {
-  console.log("voice state update");
+client.on('voiceStateUpdate', (stateOld, stateNew) => {
+  console.log('voice state updated');
 
-  for (const channelNotify of channelsNotify) {
-    const channelIdNew = stateNew.channelID;
-    const channelIdOld = stateOld.channelID;
+  const channelIdNew = stateNew.channelID;
+  const channelIdOld = stateOld.channelID;
 
-    let kind = null;
-    let channel = null;
-    let state = null;
+  let action = null;
+  let vc = null;
 
-    if (!channelIdOld && channelIdNew) {
-      kind = "in";
-      channel = client.channels.cache.get(channelIdNew);
-      state = stateNew;
-    } else if (!channelIdNew) {
-      kind = "out";
-      channel = client.channels.cache.get(channelIdOld);
-      state = stateOld;
-    }
-
-    if (kind && channel.guild.id === channelNotify.guild.id) {
-      const username = client.guilds.cache
-        .get(channel.guild.id)
-        .members.cache.get(state.id).user.username;
-
-      const userIds = Array.from(channel.guild.voiceStates.cache.keys());
-      const usernames = userIds
-        .map(userId => channel.guild.members.cache.get(userId).user.username)
-        .filter(x => (kind == "out" ? x !== username : true));
-      channelNotify.send(
-        `VC/${channel.name}/${kind}/${username}\n\\> {${usernames.join(", ")}}`
-      );
-    }
+  // connect
+  if (!channelIdOld && channelIdNew) {
+    action = 'in';
+    vc = client.channels.cache.get(channelIdNew);
+    state = stateNew;
+  }
+  // disconnect
+  else if (channelIdOld && !channelIdNew) {
+    action = 'out';
+    vc = client.channels.cache.get(channelIdOld);
+    state = stateOld;
   }
 
-  console.log("/voice state update");
+  if (action)
+    for (const { tc, vcIds } of tcs)
+      if (vcIds.includes(vc.guild.id)) {
+        const username = client.guilds.cache
+          .get(vc.guild.id)
+          .members.cache.get(state.id).user.username;
+
+        const usernames = Array.from(channel.guild.voiceStates.cache.keys())
+          .map(userId => vc.guild.members.cache.get(userId).user.username)
+          .filter(x => (action == 'out' ? x !== username : true));
+        tc.send(
+          `VC/${channel.name}/${action}/${username}\n> {${usernames.join(', ')}}`
+        );
+      }
 });
+
+client.login(process.env.DISCORD_TOKEN);
