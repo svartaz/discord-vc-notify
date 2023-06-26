6805eeae40cffd0c30c0b9f1e6834c7c0b414ec2
sumi <u22696@gmail.com>
2023-06-26 15:29:28 +0900

    handle error

--- src/main.ts
+++ src/main.ts
@@ -18,36 +18,40 @@ client.on('ready', () => {
 });
 
 const sendStateChange = (state: VoiceState, leaves: boolean) => {
-  const guild = state.guild;
-  const voiceChannelId = state.channelId;
-  const voiceChannel = <VoiceChannel>guild.channels.cache.get(<string>voiceChannelId);
-  const userIds = voiceChannel.members.map((_, userId) => userId)
+  try {
+    const guild = state.guild;
+    const voiceChannelId = state.channelId;
+    const voiceChannel = <VoiceChannel>guild.channels.cache.get(<string>voiceChannelId);
+    const userIds = voiceChannel.members.map((_, userId) => userId)
 
-  const user = guild.members.cache.get(state.id)
-  console.log(`${user?.displayName} (${user?.id}) ${leaves ? 'leaves' : 'enters'} ${voiceChannel.name} (${voiceChannel.id})`)
+    const user = guild.members.cache.get(state.id)
+    console.log(`${user?.displayName} (${user?.id}) ${leaves ? 'leaves' : 'enters'} ${voiceChannel.name} (${voiceChannel.id})`)
 
-  const embed = {
-    color: leaves ? 0xFF0000 : 0x00FF00,
-    fields: [
-      {
-        name: 'channel',
-        value: `<#${voiceChannelId}>`,
-        inline: true,
-      },
-      {
-        name: 'event',
-        value: `<@${state.id}> ${leaves ? 'out' : 'in'}`,
-        inline: true,
-      },
-      {
-        name: 'result',
-        value: userIds.map(x => `<@${x}>`).join(' ') || 'none',
-        inline: true,
-      },
-    ]
+    const embed = {
+      color: leaves ? 0xFF0000 : 0x00FF00,
+      fields: [
+        {
+          name: 'channel',
+          value: `<#${voiceChannelId}>`,
+          inline: true,
+        },
+        {
+          name: 'event',
+          value: `<@${state.id}> ${leaves ? 'out' : 'in'}`,
+          inline: true,
+        },
+        {
+          name: 'result',
+          value: userIds.map(x => `<@${x}>`).join(' ') || 'none',
+          inline: true,
+        },
+      ]
+    }
+
+    voiceChannel.send({ embeds: [embed] }).catch(console.error)
+  } catch (e) {
+    console.error(e);
   }
-
-  voiceChannel.send({ embeds: [embed] }).catch(console.error)
 }
 
 client.on('voiceStateUpdate', async (stateOld, stateNew) => {
